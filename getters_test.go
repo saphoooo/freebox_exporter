0133d114e5e6d6846b5d25d5bb4d4ccbf067da0f
Stephane Beuret <stephane.beuret@data-essential.com>
2019-07-28 11:43:18 +0200

    added testGetTemp

--- getters_test.go
+++ getters_test.go
@@ -162,3 +162,86 @@ func TestGetDsl(t *testing.T) {
 		t.Errorf("Expected 12 34 56 78, but got %v %v %v %v\n", r1, r2, s1, s2)
 	}
 }
+
+func TestGetTemp(t *testing.T) {
+	os.Setenv("FREEBOX_TOKEN", "IOI")
+	defer os.Unsetenv("FREEBOX_TOKEN")
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case "/good":
+			myRRD := rrd{
+				Success: true,
+			}
+			myRRD.Result.Data = []map[string]int{
+				map[string]int{
+					"cpum":      01,
+					"cpub":      02,
+					"sw":        03,
+					"hdd":       04,
+					"fan_speed": 05,
+				},
+			}
+			result, _ := json.Marshal(myRRD)
+			fmt.Fprintln(w, string(result))
+		case "/error":
+			myRRD := rrd{
+				Success:   true,
+				ErrorCode: "denied_from_external_ip",
+			}
+			result, _ := json.Marshal(myRRD)
+			fmt.Fprintln(w, string(result))
+		case "/null":
+			myRRD := rrd{
+				Success: true,
+			}
+			result, _ := json.Marshal(myRRD)
+			fmt.Fprintln(w, string(result))
+		}
+	}))
+	defer ts.Close()
+
+	goodPR := &postRequest{
+		method: "POST",
+		header: "X-Fbx-App-Auth",
+		url:    ts.URL + "/good",
+	}
+
+	errorPR := &postRequest{
+		method: "POST",
+		header: "X-Fbx-App-Auth",
+		url:    ts.URL + "/error",
+	}
+
+	nullPR := &postRequest{
+		method: "POST",
+		header: "X-Fbx-App-Auth",
+		url:    ts.URL + "/null",
+	}
+
+	ai := &authInfo{}
+	mySessionToken := "foobar"
+
+	cpum, cpub, sw, hdd, fanSpeed, err := getTemp(ai, goodPR, &mySessionToken)
+	if err != nil {
+		t.Error("Expected no err, but go", err)
+	}
+
+	if cpum != 01 || cpub != 02 || sw != 03 || hdd != 04 || fanSpeed != 05 {
+		t.Errorf("Expected 01 02 03 04 05, but got %v %v %v %v %v\n", cpum, cpub, sw, hdd, fanSpeed)
+	}
+
+	cpum, cpub, sw, hdd, fanSpeed, err = getTemp(ai, errorPR, &mySessionToken)
+	if err.Error() != "You are trying to get an app_token from a remote IP" {
+		t.Error("Expected You are trying to get an app_token from a remote IP, but go", err)
+	}
+
+	cpum, cpub, sw, hdd, fanSpeed, err = getTemp(ai, nullPR, &mySessionToken)
+	if err != nil {
+		t.Error("Expected no err, but go", err)
+	}
+
+	if cpum != 0 || cpub != 0 || sw != 0 || hdd != 0 || fanSpeed != 0 {
+		t.Errorf("Expected 01 02 03 04 05, but got %v %v %v %v %v\n", cpum, cpub, sw, hdd, fanSpeed)
+	}
+}
